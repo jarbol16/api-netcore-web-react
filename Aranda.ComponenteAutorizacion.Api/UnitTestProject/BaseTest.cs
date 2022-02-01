eb6e9accefd786902777cf9255454166cb78b0ea
Daniel Arboleda <jarbol16@eafit.edu.co>
2022-02-01 00:34:12 -0500

    Se agrega proyetco de pruebas unitarias
    se realiza pruebas unitarias, dado que no hay logica de negocia se realiza sobre accesos a datos

---
+++ Aranda.ComponenteAutorizacion.Api/UnitTestProject/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestProject
+{
+    public class BaseTest
+    {
+
+        public static DbSet<T> GetQueryableMockDbSet<T>(List<T> sourceList) where T : class, new()
+        {
+            var queryable = sourceList.AsQueryable();
+            var mockSet = new Mock<DbSet<T>>() { CallBase = true };
+            mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
+
+            return mockSet.Object;
+        }
+    }
+}
---
+++ Aranda.ComponenteAutorizacion.Api/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,87 @@
+using Aranda.ComponenteAutorizacion.Api.Controllers;
+using Aranda.ComponenteAutorizacion.Entities;
+using BusinessRules;
+using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using Utilities;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly  Mock<MainContext> mockDbContext;
+        private readonly Mock<IUserBusiness> mockUserBusiness;
+        private readonly Mock<IRepository> mockRepository;
+        public UnitTest1()
+        {
+            mockDbContext = new Mock<MainContext>();
+            mockUserBusiness = new Mock<IUserBusiness>();
+            mockRepository = new Mock<IRepository>();
+        }
+
+        [TestMethod]
+        public void GetUsersTest()
+        {
+            List<Usuario> users = new List<Usuario>
+            {
+                new Usuario
+                {
+                    Id=1,
+                    Nombre="admon",
+                    Contrasena ="123"
+                },
+                new Usuario
+                {
+                    Id=2,
+                    Nombre="dev",
+                    Contrasena="456"
+                }
+            };
+            mockDbContext.Setup(x => x.Usuarios).Returns(BaseTest.GetQueryableMockDbSet<Usuario>(users));
+
+
+            var service = new Repository(mockDbContext.Object);
+            var response = service.GetUsers();
+
+            Assert.AreEqual(users.Count, response.Count);
+        }
+
+
+        [TestMethod]
+        public void ValidateUserControllerTest()
+        {
+            Usuario user = new Usuario
+            {
+                Id = 1,
+                Nombre = "admon",
+                Persona = new Persona
+                {
+                    Nombre="Daniel"
+                }
+            };
+
+            UserCredentials credentials = new UserCredentials
+            {
+                UserName = "admon",
+                Password = "123"
+            };
+
+            mockUserBusiness.Setup(x => x.ValidateLogin(credentials)).Returns(user);
+
+            UserController userController = new UserController(mockUserBusiness.Object);
+            var response =  userController.UserValidate(credentials);
+            var objResult = response as ObjectResult;
+            var objResult2 = objResult.Value;
+
+            Assert.AreEqual(user, objResult2);
+
+        }
+
+
+    }
+}
